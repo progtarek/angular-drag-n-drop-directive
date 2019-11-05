2cd272d999e2a788f67a946b7eb7351fdb4ede1f
tarek <tarek.noaman@qurba.io>
2019-11-05 11:27:18 +0200

    [Directives] add drag and drop with functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dnd';
+  /**
+   * on file drop handler
+   */
+  onFileDropped($event) {
+    console.log('event', $event);
+    window.alert(`I got ${$event.length} files.`);
+  }
+
+  /**
+   * handle file from browsing
+   */
+  fileBrowseHandler(files) {
+    console.log('files', files);
+    window.alert(`I got ${files.length} files.`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DndDirective } from './direcitves/dnd.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DndDirective
   ],
   imports: [
     BrowserModule
